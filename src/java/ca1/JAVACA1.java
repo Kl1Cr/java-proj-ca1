b0c7cfec681d072f4152e542457dcee52917e505
kamal <ganesankamalakshay@gmaul.com>
2025-06-11 16:44:32 +0800

    Second Commit with a message

--- src/java/ca1/JAVACA1.java
+++ src/java/ca1/JAVACA1.java
@@ -15,6 +15,7 @@ public class JAVACA1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Welcome to Java-CA1");
     }
     
 }
