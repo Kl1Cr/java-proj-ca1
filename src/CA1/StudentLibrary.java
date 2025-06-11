930db766540a868d83d704fdd0642df10dea4837
kamal <ganesankamalakshay@gmaul.com>
2025-06-12 01:18:47 +0800

    Just started on the create student.

--- src/CA1/StudentLibrary.java
+++ src/CA1/StudentLibrary.java
@@ -17,6 +17,7 @@ public class StudentLibrary {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        while(true){
        String userOption = JOptionPane.showInputDialog(null, 
                 "Enter your option:\n" +
                 "1. Student management\n" +
@@ -24,24 +25,69 @@ public class StudentLibrary {
                 "3. Exit",
                 "Mini Library System",
                 JOptionPane.QUESTION_MESSAGE);
-        int option = Integer.parseInt(userOption);
         
+       int option = Integer.parseInt(userOption);
+        
+        if(userOption == null){
+        JOptionPane.showMessageDialog(null, 
+                "Program terminated. \nThank You!",
+                "Message",
+                JOptionPane.INFORMATION_MESSAGE);
+             break;
+        }
         switch (option) {
-            case 3:
+            case 1:
+                boolean isStudentOption = true;
+                while(isStudentOption){
+                String studentOption = JOptionPane.showInputDialog(null, 
+                "Enter your option:\n" +
+                "1. Display all students\n" +
+                "2. Search students by name\n" +
+                "3. Add new student\n"+
+                "4. Display total number of students \n" +
+                "5. Exit\n",
+                "Mini Library System",
+                JOptionPane.QUESTION_MESSAGE);
+                int student = Integer.parseInt(studentOption);
+                if(student==1){
+                //To do
+                }else if(student==2){
+                //Call method or something here 
+                }else if(student==3){
+                //Call method or something here 
+                }else if(student==4){
+                //Call method or something here 
+                }else if(studentOption == null){
+                isStudentOption = false;
+                continue;
+                }else if(student==5){
+                isStudentOption=false;
+                }else{
                 JOptionPane.showMessageDialog(null,
+                                "Invalid option. Please try again.",
+                                "Error",
+                                JOptionPane.ERROR_MESSAGE);
+                }
+                }
+                break;
+            case 2:
+                //To do code ah boy thompson
+                break;
+             case 3:
+                    JOptionPane.showMessageDialog(null,
                         "Program terminated.\nThank you!",
                         "Message",
                         JOptionPane.INFORMATION_MESSAGE);
-                break;
-        //To do code
-            case 1:
-                break;
-        //To do code
-            case 2:
-                break;
-            default:
-                break;
+                    System.exit(0);
+                    break;
+             default:
+                    JOptionPane.showMessageDialog(null,
+                        "Invalid option. Please try again.",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+                    break;
         }
+    }
     }
     
 }
---
+++ src/CA1/addStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA1;
+
+/**
+ *
+ * @author Kamal
+ */
+public class addStudent {
+    private String name,admin_num;
+    public addStudent(){
+    this.name = " ";
+    this.admin_num = " ";
+    }
+    
+    public addStudent(String name, String admin_num){
+    this.name = name;
+    this.admin_num = admin_num;
+    }
+    public void setStudent(String name, String admin_num){
+    this.name = name;
+    this.admin_num =admin_num;
+    }
+}
