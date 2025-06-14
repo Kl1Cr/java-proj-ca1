e6e59ac9f320df1c6e121bcd392412935a8532a3
kamal <ganesankamalakshay@gmaul.com>
2025-06-15 01:32:15 +0800

    Edited adding student and displaying the students

--- src/CA1/addStudent.java
+++ src/CA1/addStudent.java
@@ -3,28 +3,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package CA1;
-
+import java.util.ArrayList;
 /**
  *
  * @author Kamal
  */
 public class addStudent {
-    private String name,admin_num;
-    public addStudent(){
-    this.name = " ";
-    this.admin_num = " ";
+   private String name, admin_num;
+    private ArrayList<String> booksBorrowed;
+
+    public addStudent() {
+        this.name = "";
+        this.admin_num = "";
+        this.booksBorrowed = new ArrayList<>();
     }
-    
-    public addStudent(String name, String admin_num){
-    this.name = name;
-    this.admin_num = admin_num;
+
+    public addStudent(String name, String admin_num) {
+        this.name = name;
+        this.admin_num = admin_num;
+        this.booksBorrowed = new ArrayList<>();
     }
-    public void setStudent(String name, String admin_num){
-    this.name = name;
-    this.admin_num =admin_num;
+
+    public void setStudent(String name, String admin_num) {
+        this.name = name;
+        this.admin_num = admin_num;
     }
+
     public String getStudent() {
-    return "Student name: " + this.name + " Admin No: " + this.admin_num;
-}
+        return "Student name: " + this.name + " Admin No: " + this.admin_num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAdminNum() {
+        return admin_num;
+    }
+
+    public ArrayList<String> getBooksBorrowed() {
+        return booksBorrowed;
+    }
+
+    public void addBookBorrowed(String bookTitle) {
+        booksBorrowed.add(bookTitle);
+    }
 
 }
